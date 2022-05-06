1ccae40af052201473f7f822d441e582cbfbf160
tykavanaugh <tykavanaugh@gmail.com>
2022-05-05 18:59:01 -0700

    error codes

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 import json
 #from .serializers import TaskSerializer
 #from .models import Task
@@ -20,7 +21,24 @@ def ping_view(request):
 @api_view(['GET'])
 def post_view(request):
     body = json.loads(request.body.decode("utf-8"))
-    if not body['tags']:
-        return Response('placeholder error')
-    return Response('test')
+    #Handle tag errors
+    if 'tags' not in body:
+        return JsonResponse({"error": "tags parameter is required"},status=status.HTTP_400_BAD_REQUEST)
+    #Default values
+    sortBy = 'id'
+    direction = 'asc'
+    #Handle invalid sortBy
+    if 'sortBy' in body:
+        if body['sortBy'] not in ['id','reads','likes','popularity']:
+            #Invalid value
+            return JsonResponse({"error": "sortBy parameter is required"},status=status.HTTP_400_BAD_REQUEST)
+        sortBy = body['sortBy']
+    #Handle invalid direction 
+    if 'direction' in body:
+        if body['direction'] not in ['desc','asc']:
+            #Invalid value
+            return JsonResponse({"error": "direction parameter is required"},status=status.HTTP_400_BAD_REQUEST)
+        direction = body['direction']
+
+    return Response(f'test {sortBy} {direction}')
 
